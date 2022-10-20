650614bd79d3922f1157138b061c5bc41520852b
Douglas Ribeiro <dgrb0325@gmail.com>
2022-10-19 21:42:43 -0300

    task completed

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -1,9 +1,9 @@
-import { getRepository, Repository } from 'typeorm';
+import { User } from "./../../../users/entities/User";
+import { getRepository, Repository } from "typeorm";
 
-import { User } from '../../../users/entities/User';
-import { Game } from '../../entities/Game';
+import { Game } from "../../entities/Game";
 
-import { IGamesRepository } from '../IGamesRepository';
+import { IGamesRepository } from "../IGamesRepository";
 
 export class GamesRepository implements IGamesRepository {
   private repository: Repository<Game>;
@@ -14,17 +14,28 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder("games")
+      .where(`games.title ILIKE '%${param}%'`)
+      .getMany();
+    // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(`SELECT COUNT(games.id) FROM games`); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const userRepository = getRepository(User);
+
+    return userRepository
+      .createQueryBuilder("users")
+      .innerJoin(
+        "users_games_games",
+        "users_games_games",
+        "users_games_games.usersId = users.id"
+      )
+      .where("users_games_games.gamesId = :pId", { pId: id })
+      .getMany();
+    // Complete usando query builder
   }
 }
